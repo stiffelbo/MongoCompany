2dee62245335b9e7cc3fc8527b6e4471c7a16826
stiffelbo <partygrouppl@gmail.com>
2021-06-15 22:11:01 +0200

    29.2 routes with db, random err

---
+++ routes/products.routes.js
@@ -0,0 +1,51 @@
+// post.routes.js
+
+const express = require('express');
+const { ObjectId } = require('mongodb');
+const router = express.Router();
+
+router.get('/products', (req, res) => {
+  req.db.collection('products').find().toArray((err, data) => {
+    if(err) res.status(500).json({message : err});
+    else res.json(data)
+  });  
+});
+
+router.get('/products/:id', (req, res) => {
+  req.db.collection('products').findOne({_id : ObjectId(req.params.id)}, (err, data) => {
+    if(err) res.status(500).json({message: "Not found"});
+    else res.json(data);
+  });
+});
+
+router.post('/products', (req, res) => {
+  const { name, client } = req.body;
+  req.db.collection('products').insertOne({name : name, client : client}, (err, data) => {
+    if(err) res.status(500).json({message: err});
+    else res.json({message: "OK"});
+  });
+});
+
+router.put('/products/:id', (req, res) => {
+  const { name, client } = req.body;
+  req.db.collection('products').updateOne({ _id: ObjectId(req.params.id) }, { $set: { name: name , client : client}}, err => {
+    if(err) res.status(500).json({ message: err });
+    else res.json({ message: 'OK' });
+  });
+});
+
+router.delete('/products/:id', (req, res) => {
+  req.db.collection('products').deleteOne({_id : ObjectId(req.params.id)}, err => {
+    if(err) res.status(500).json({ message: err });
+    else res.json({ message: 'OK' });
+  });
+});
+
+router.get('/products/random', (req, res) => {
+  req.db.collection('products').aggregate([ { $sample: { size: 1 } } ]).toArray((err, data) => {
+    if(err) res.status(500).json({ message: err });
+    else res.json(data[0]);
+  });
+});
+
+module.exports = router;
