804db5a1586286118ba1ea242812222c8c11289b
stiffelbo <partygrouppl@gmail.com>
2021-06-21 17:01:11 +0200

    30.4 Delete test fixed

--- test/api/departments/delete.test.js
+++ test/api/departments/delete.test.js
@@ -17,7 +17,7 @@ describe('DELETE /api/departments', () => {
   it('/:id should delete chosen document and return success', async () => {
     const res = await request(server).delete('/api/departments/5d9f1140f10a81216cfd4408'); 
     expect(res.status).to.be.equal(200);
-    expect(res.body).to.not.be.equal({ _id: '5d9f1140f10a81216cfd4408', name: 'Department #1' });   
+    expect(res.body).to.have.property('_id').that.is.a('string');
   });
 
   after(async () => {
